6d42d32e484f8c14a1fbffaca4de8b09fa025394
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2017-04-08 08:43:00 +0200

    Fix empty lines

--- lib/read_books.rb
+++ lib/read_books.rb
@@ -25,56 +25,58 @@ file_paths.each do |file_path|
 
       ### Check for metadata
       # Check for title
-      if line[0..6] == "Title: "
-        title = line[6..-1].strip
-        puts "Found title: #{title}"
-        current_book = Book.where(:title => title).first || Book.new({title: title}) if current_book.nil?
-        book_exists = !!current_book.id
-        puts book_exists ? "Book exists in database." : "Creating book ..."
-        current_book.save
-        # Check for author
-      elsif line[0..7] == "Author: " && current_book.author.nil?
-        author_name = line[8..-1].strip
-        puts "Found author: #{author_name}"
-        current_author = Author.where(:name => author_name).first || Author.new({name: author_name})
-        current_author.save
-        if !current_book.nil?
-          current_book.author = current_author
+      if !line.blank?
+        if line[0..6] == "Title: "
+          title = line[6..-1].strip
+          puts "Found title: #{title}"
+          current_book = Book.where(:title => title).first || Book.new({title: title}) if current_book.nil?
+          book_exists = !!current_book.id
+          puts book_exists ? "Book exists in database." : "Creating book ..."
+          current_book.save
+          # Check for author
+        elsif line[0..7] == "Author: " && current_book.author.nil?
+          author_name = line[8..-1].strip
+          puts "Found author: #{author_name}"
+          current_author = Author.where(:name => author_name).first || Author.new({name: author_name})
+          current_author.save
+          if !current_book.nil?
+            current_book.author = current_author
+            current_book.save
+          end
+          # Check for translator
+        elsif line[0..11] == "Translator: " && current_book.translator.nil?
+          translator = line[12..-1].strip
+          puts "Found translator: #{translator}"
+          current_book.translator = translator
+          current_book.save
+          # Check for release date
+        elsif line[0..13] == "Release Date: " && current_book.release_date.nil?
+          # Parse string to date
+          date = line[14..-1].match(/(\w+\s\d,\s\d+)/)[1].strip
+          puts "Found release date: #{date}"
+          current_book.release_date = Date.parse(date)
           current_book.save
         end
-        # Check for translator
-      elsif line[0..11] == "Translator: " && current_book.translator.nil?
-        translator = line[12..-1].strip
-        puts "Found translator: #{translator}"
-        current_book.translator = translator
-        current_book.save
-        # Check for release date
-      elsif line[0..13] == "Release Date: " && current_book.release_date.nil?
-        # Parse string to date
-        date = line[14..-1].match(/(\w+\s\d,\s\d+)/)[1].strip
-        puts "Found release date: #{date}"
-        current_book.release_date = Date.parse(date)
-        current_book.save
-      end
     
-      if book_exists && !count_reset_to_default 
-        puts "Resetting all occurrences counts to 0 ..."
-        Occurrence.where(:book_id => current_book.id).to_a.each do |occurrence|
-          occurrence.count = 0
-          occurrence.save
-        end
-        count_reset_to_default = true
-      end
-    
-      ### Check for cities
-      words = line.split
-      words.each do |word|
-        if !current_book.nil?
-          city = City.where(:name => word).first
-          if !city.nil?
-            occurrence = Occurrence.where(:city_id => city.id, :book_id => current_book.id).first || Occurrence.new({city: city, book: current_book})
-            occurrence.count = occurrence.count + 1
+        if book_exists && !count_reset_to_default 
+          puts "Resetting all occurrences counts to 0 ..."
+          Occurrence.where(:book_id => current_book.id).to_a.each do |occurrence|
+            occurrence.count = 0
             occurrence.save
+          end
+          count_reset_to_default = true
+        end
+    
+        ### Check for cities
+        words = line.split
+        words.each do |word|
+          if !current_book.nil?
+            city = City.where(:name => word).first
+            if !city.nil?
+              occurrence = Occurrence.where(:city_id => city.id, :book_id => current_book.id).first || Occurrence.new({city: city, book: current_book})
+              occurrence.count = occurrence.count + 1
+              occurrence.save
+            end
           end
         end
       end
