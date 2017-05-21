baec520dd1c9609631d9f9db5afaee8d4cba33e0
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2017-05-21 10:17:53 +0200

    Diverse

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -2,7 +2,7 @@ class Occurrence < ActiveRecord::Base
   belongs_to :book
   belongs_to :city
   field :count, as: :integer, default: 0
-  index :book_id
-  index :city_id
+  #index :book_id
+  #index :city_id
 end
 Occurrence.auto_upgrade!
--- lib/load_books.rb
+++ lib/load_books.rb
@@ -18,18 +18,25 @@ current_book_no = File.exist?(line_status_file) ? (File.open(line_status_file) {
 avg_runtimes = []
 
 puts "Found #{file_paths.length} books."
+
 file_paths.each_with_index do |file_path, index|
-  next if index < current_book_no - 1
-  current_book_no = current_book_no + 1
+  
+  next if index < current_book_no
   File.write(line_status_file, index.to_s)
-  puts "Reading (#{current_book_no} / #{book_count}) #{file_path} ..."
+  
+  
+  puts "Reading (#{current_book_no + 1} / #{book_count}) #{file_path} ..."
   raise "File not found." unless File.exists?(file_path)
   time_start = Time.now
-  current_book = nil
+  
+  current_book = Book.where(:file_path => file_path).first
+  book_exists = !current_book.nil?
+  current_book = Book.new({:file_path => file_path}) if current_book.nil?
+  current_book.save
+  
   current_author = nil
   #line_count = %x{wc -l #{file_path}}.split[0].to_i
   current_line_no = 1
-  book_exists = false
   count_reset_to_default = false
 
   File.open(file_path, "r:UTF-8") do |f|
@@ -37,36 +44,29 @@ file_paths.each_with_index do |file_path, index|
       line = line.scrub
       current_line_no = current_line_no + 1
       #puts "Reading line: #{current_line_no} / #{line_count}" if current_line_no % 1000 == 0
-
       ### Check for metadata
       # Check for title
       if !line.blank?
-        if line.length > 6 && line[0..6] == "Title: "
+        if current_book.title.blank? && line.length > 6 && line[0..6] == "Title: "
           title = line[6..-1].strip
           puts "Found title: #{title}"
-          current_book = Book.where(:title => title).first || Book.new({title: title}) if current_book.nil?
-          book_exists = !!current_book.id
-          puts book_exists ? "Book exists in database." : "Creating book ..."
-          current_book.file_path = file_path
-          current_book.save
+          current_book.title = title
+
           # Check for author
-        elsif !current_book.nil? && line.length > 8 && line[0..7] == "Author: " && current_book.author.nil?
+        elsif current_book.author.blank? && line.length > 8 && line[0..7] == "Author: "
           author_name = line[8..-1].strip
           puts "Found author: #{author_name}"
           current_author = Author.where(:name => author_name).first || Author.new({name: author_name})
           current_author.save
-          if !current_book.nil?
-            current_book.author = current_author
-            current_book.save
-          end
+          current_book.author = current_author
+          
           # Check for translator
-        elsif !current_book.nil? && line.length > 12 &&  line[0..11] == "Translator: " && current_book.translator.nil?
+        elsif current_book.translator.blank? && line.length > 12 &&  line[0..11] == "Translator: "
           translator = line[12..-1].strip
           puts "Found translator: #{translator}"
           current_book.translator = translator
-          current_book.save
           # Check for release date
-        elsif !current_book.nil? && line.length > 14 && line[0..13] == "Release Date: " && current_book.release_date.nil?
+        elsif current_book.release_date.blank? && line.length > 14 && line[0..13] == "Release Date: "
           # Parse string to date
           begin
             date_str = line[14..-1]
@@ -78,7 +78,7 @@ file_paths.each_with_index do |file_path, index|
             if !date.blank?
               puts "Found release date: #{date}"
               current_book.release_date = Date.parse(date)
-              current_book.save
+              
             end
           rescue Exception => e
             puts "Failed to parse date ..."
@@ -109,15 +109,18 @@ file_paths.each_with_index do |file_path, index|
     end
   end
 
-  if !current_book.nil?
-    hits = Occurrence.where(:book_id => current_book.id).count
-    puts "Found #{hits} cities in #{current_book.title}."
-  end
+  current_book.save
+  
+  #hits = Occurrence.where(:book_id => current_book.id).count
+  #puts "Found #{hits} cities in #{current_book.title}."
+  
   elapsed_time = Time.now - time_start
   elapsed_time_in_seconds = ((elapsed_time % 3600) % 60).to_i
   avg_runtimes << elapsed_time_in_seconds
   avg_time = avg_runtimes.inject{ |sum, el| sum + el }.to_f / avg_runtimes.size
-  puts "Elapsed time (seconds): #{elapsed_time_in_seconds}"
+  
+  #puts "Elapsed time (seconds): #{elapsed_time_in_seconds}"
   puts "Average reading time (seconds): #{avg_time}"
+  current_book_no = current_book_no + 1
 end
 puts "Total elapsed time (seconds): #{avg_runtimes.inject{ |sum, el| sum + el }.to_f}\n"
